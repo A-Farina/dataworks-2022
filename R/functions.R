46b8d32a344af14c5a2162eabe62bfa6e362d2a5
A-Farina <42683113+A-Farina@users.noreply.github.com>
2022-04-27 15:42:46 -0400

    Updated post briefing

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,6 @@
 clean_data <- function(df) {
   clean_data <- df %>%
-    mutate(fueltype = as.factor(fueltype),
-           price = price/1000)
+    mutate(fueltype = as.factor(fueltype))
 
   return(clean_data)
 }
