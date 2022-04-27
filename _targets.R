c765b9a926b23866eca0bd7500d7b0eaa52afb92
A-Farina <42683113+A-Farina@users.noreply.github.com>
2022-04-27 12:09:17 -0400

    executed tar_destroy() to reset the files for the walk-through

--- _targets.R
+++ _targets.R
@@ -10,7 +10,8 @@ list(
   tar_target(raw_data, rio::import(raw_data_file)),
   tar_target(data, clean_data(raw_data)),
   tar_target(linear_model, model(data)),
-  tar_target(tidy_model, model2(data)),
-  tar_render(report, "R/markdown.rmd"),
-  tar_render(slides, "presentation/index.rmd")
+#  tar_target(tidy_model, model2(data)),
+#  tar_render(report, "R/markdown.rmd"),
+#  tar_render(slides, "presentation/index.rmd"),
+  NULL
 )
